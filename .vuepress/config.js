18d378cb1096d18c98b84c7a7032d457a5e1f0ce
han1396735592 <1396735592@qq.com>
2019-03-18 16:47:36 +0800

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    title: '我的学习笔记',
+    description: 'Just playing around',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ], themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+                items: [
+                    { text: 'Android', link: '/android/' },
+                    { text: 'ios', link: '/ios/' },
+                    { text: 'Web', link: '/web/' }
+                ]
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+        sidebar: {
+            '/android/': [
+                "",
+                "android1",
+            ],
+            "/ios/":[
+                "",
+                "ios1",
+            ],
+            "/web/":[
+                "",
+                "web1",
+            ],
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+    }
+}
